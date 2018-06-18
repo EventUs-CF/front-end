c725201e8c04fec09b2d53fcac3f4c8123fd4e56
LACY HOGAN <lacy.hogan@gmail.com>
2018-06-18 10:57:46 -0700

    Initial scaffolding

---
+++ src/actions/auth.js
@@ -0,0 +1,39 @@
+import superagent from 'superagent';
+import * as routes from '../routes';
+import { deleteCookie } from '../utils/cookie';
+
+const TOKEN_COOKIE_KEY = 'PPA-Token';
+
+export const setTokenAction = token => ({
+  type: 'TOKEN_SET',
+  payload: token,
+});
+
+export const removeTokenAction = () => ({
+  type: 'TOKEN_REMOVE',
+});
+
+export const logout = () => {
+  deleteCookie(TOKEN_COOKIE_KEY);
+  return removeTokenAction();
+};
+
+export const signupRequest = user => (store) => {
+  return superagent.post(`${API_URL}${routes.SIGNUP_ROUTE}`)
+    .send(user)
+    .withCredentials()
+    .then((response) => {
+      return store.dispatch(setTokenAction(response.text));
+    });
+};
+
+export const loginRequest = user => (store) => {
+  return superagent.get(`${API_URL}${routes.LOGIN_ROUTE}`)
+    .auth(user.username, user.password)
+    .withCredentials()
+    .then((response) => {
+      // console.log(response);
+      // console.log(response.text);
+      return store.dispatch(setTokenAction(response.text));
+    });
+};
---
+++ src/utils/auto-bind.js
@@ -0,0 +1,8 @@
+export default function autoBind(classComponent) {
+  const classMethods = Object.getOwnPropertyNames(classComponent.prototype);
+  classMethods.forEach((method) => {
+    if (method.startsWith('handle')) {
+      this[method] = this[method].bind(this);
+    }
+  });
+}
