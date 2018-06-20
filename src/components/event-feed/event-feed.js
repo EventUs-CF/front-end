dc0f4534121a06e7043c1b32a00cd91b51cac0be
Deliman206 <smiller206@me.com>
2018-06-20 15:40:52 -0700

    passed events from store to event feed

--- src/components/event-feed/event-feed.js
+++ src/components/event-feed/event-feed.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class EventFeed extends React.Component {
+class EventFeed extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   componentDidMount() {
     window.addEventListener('scroll', this.onScroll, false);
+    this.setState({ eventList: this.props.event[0] });
   }
 
   componentWillUnmount() {
@@ -16,11 +18,19 @@ export default class EventFeed extends React.Component {
 
   onScroll = () => {
     if ((window.innerHeight + window.scrollY) >= 
+    // this.props.eventList.length SHOULD come from UI state
     (document.body.offsetHeight - 500) && this.props.eventList.length) {
       // New or different method here? 
       // do we want pagination search? 
       // should call the method to render/draw new items from the array 
+
+      // this is not a function in our program 
       this.props.onPaginatedSearch();
+
+
+      // Here we need ot make a call to state fro all events
+      // store result from above  [] => eventList []
+      // result [[{},{},{}]] @ [0] this.setState({ eventList = result[0] })
     }
   }
 
@@ -42,4 +52,11 @@ export default class EventFeed extends React.Component {
 EventFeed.propTypes = {
   eventList: PropTypes.array,
   onPaginatedSearch: PropTypes.func,
+  event: PropTypes.array,
 };
+
+const mapStateTopProps = state => ({
+  event: state.event,
+});
+
+export default connect(mapStateTopProps, null)(EventFeed);
