fe8fcab073aab32922d0dca35859b8bca9df1fd1
vibecastor <vibecastor@gmail.com>
2018-06-20 18:21:55 -0700

    webpack configs for deployment - using Vinicios configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,42 +1,31 @@
+
 'use strict';
 
 require('dotenv').config();
 
-const { DefinePlugin } = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssPlugin = require('mini-css-extract-plugin');
-
-const production = process.env.NODE_ENV === 'production';
 
 const webpackConfig = module.exports = {};
 
 webpackConfig.entry = `${__dirname}/src/main.js`;
 
 webpackConfig.output = {
-  filename: '[title].[hash].js',
+  filename: '[name].[hash].js',
   path: `${__dirname}/build`,
   publicPath: process.env.CDN_URL,
 };
 
 webpackConfig.plugins = [
   new HtmlWebpackPlugin({
-    title: 'EventUs',
+    title: '401d23 Budget Tracker',
   }),
-  new DefinePlugin({
-    API_URL: JSON.stringify(process.env.API_URL),
+  new MiniCssPlugin({
+    filename: '[name].[hash].css',
   }),
 ];
 
-if (production) {
-  webpackConfig.plugins.push(new MiniCssPlugin({
-    filename: '[title].[hash].css',
-  }));
-}
-
 webpackConfig.module = {};
-
-const finalLoader = production ? MiniCssPlugin.loader : 'style-loader';
-
 webpackConfig.module.rules = [
   {
     test: /\.(png|svg|jpg|gif)$/,
@@ -55,13 +44,5 @@ webpackConfig.module.rules = [
         cacheDirectory: true,
       },
     },
-  },
-  {
-    test: /\.s?css$/,
-    use: [
-      finalLoader,
-      'css-loader',
-      'sass-loader',
-    ],
   },
 ];
