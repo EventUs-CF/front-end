cd06cac3cf172875db70e7b760ce6426754f1885
Deliman206 <smiller206@me.com>
2018-06-20 20:14:52 -0700

    added slashes to api url in actions

--- src/actions/event.js
+++ src/actions/event.js
@@ -26,7 +26,7 @@ export const eventsFetch = events => ({
 const createRequest = event => (store) => {
   const { token, user } = store.getState();
   event.createdBy = user._id;
-  return superagent.post(`${API_URL}${routes.EVENT_ROUTE}`)
+  return superagent.post(`${API_URL}/${routes.EVENT_ROUTE}`)
     .set('Content-Type', 'application/json')
     .set('Authorization', `Bearer ${token.split('"')[3]}`)
     .send(event)
@@ -38,7 +38,7 @@ const createRequest = event => (store) => {
 const updateRequest = event => (store) => {
   const { token } = store.getState();
 
-  return superagent.put(`${API_URL}${routes.EVENT_ROUTE}/${event._id}`)
+  return superagent.put(`${API_URL}/${routes.EVENT_ROUTE}/${event._id}`)
     .set('Authorization', `Bearer ${token}`)
     .set('Content-Type', 'application/json')
     .send(event)
@@ -50,7 +50,7 @@ const updateRequest = event => (store) => {
 const deleteRequest = event => (store) => {
   const { token } = store.getState();
 
-  return superagent.delete(`${API_URL}${routes.EVENT_ROUTE}/${event._id}`)
+  return superagent.delete(`${API_URL}/${routes.EVENT_ROUTE}/${event._id}`)
     .set('Authorization', `Bearer ${token}`)
     .then((response) => {
       return store.dispatch(eventDelete(response.body));
@@ -60,7 +60,7 @@ const deleteRequest = event => (store) => {
 const fetchRequest = () => (store) => {
   const { token } = store.getState();
 
-  return superagent.get(`${API_URL}${routes.EVENT_ROUTE}`)
+  return superagent.get(`${API_URL}/${routes.EVENT_ROUTE}`)
     .set('Authorization', `Bearer ${token}`)
     .then((response) => {
       return store.dispatch(eventsFetch(response.body));
--- src/actions/user.js
+++ src/actions/user.js
@@ -11,7 +11,7 @@ const setProfile = user => ({
 const createRequest = user => (store) => {
   const { token } = store.getState();
 
-  return superagent.post(`${API_URL}${routes.USER_ROUTE}`)
+  return superagent.post(`${API_URL}/${routes.USER_ROUTE}`)
     .set('Authorization', `Bearer ${token.split('"')[3]}`)
     .set('Content-Type', 'application/json')
     .send(user)
@@ -23,7 +23,7 @@ const createRequest = user => (store) => {
 const updateRequest = user => (store) => {
   const { token } = store.getState();
 
-  return superagent.put(`${API_URL}${routes.USER_ROUTE}/${user._id}`)
+  return superagent.put(`${API_URL}/${routes.USER_ROUTE}/${user._id}`)
     .set('Authorization', `Bearer ${token}`)
     .set('Content-Type', 'application/json')
     .send(user)
@@ -35,7 +35,7 @@ const updateRequest = user => (store) => {
 const fetchRequest = () => (store) => {
   const { token } = store.getState();
 
-  return superagent.get(`${API_URL}${routes.USER_ROUTE}`)
+  return superagent.get(`${API_URL}/${routes.USER_ROUTE}`)
     .set('Authorization', `Bearer ${token}`)
     .then((response) => {
       return store.dispatch(setProfile(response.body));
