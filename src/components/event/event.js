2f63c150d9f1925a858de157e44419146e481227
Deliman206 <smiller206@me.com>
2018-06-22 11:06:15 -0700

    uncomment event item code

--- src/components/event/event.js
+++ src/components/event/event.js
@@ -24,7 +24,7 @@ export default class EventPost extends React.Component {
     return (
       <div>
           <div className='eventPost' onClick={showModal}>
-            <aside>6:30 PM</aside>
+            <aside>{this.props.event.time}</aside>
             <div className='eventInfo'>
               <div className="eventHeader"></div>
               <div className='location'>{this.props.event.location}</div>
@@ -34,20 +34,20 @@ export default class EventPost extends React.Component {
 
           <Modal className='modal' show={this.state.detail} hide={hideModal}>
             <div>
-            <div className='modalDate'>Friday, June 22, 2018</div>
+            {/* <div className='modalDate'>Friday, June 22, 2018</div> */}
 
-              {/* <div className='modalDate'>{this.props.event.date}</div> */}
+              <div className='modalDate'>{this.props.event.date}</div>
               <div className='modalTitle'><b>{this.props.event.title}</b></div>
               <p>Are You Going?</p>
               <button className='confirm'>confirm</button>
               <button className='deny'>deny</button>
               <hr/>
               <div className='info'>
-                {/* <div className='modalDate'>{this.props.event.date}</div> */}
-                <div className='date2'>Friday, June 22, 2018</div>
+                <div className='modalDate'>{this.props.event.date}</div>
+                {/* <div className='date2'>Friday, June 22, 2018</div> */}
 
-                {/* <div className='time'>{this.props.event.time}</div> */}
-                <div className='time'>1:00 PM</div>
+                <div className='time'>{this.props.event.time}</div>
+                {/* <div className='time'>1:00 PM</div> */}
 
                 <div className='modalLocation'>{this.props.event.location}</div>
                 <div className='modalCost'>$ {this.props.event.cost}</div>
@@ -55,8 +55,8 @@ export default class EventPost extends React.Component {
               <Map address={this.props.event.address}/>
               {/* add api call for geo location -> lat and Long */}
               <h3>Details</h3>
-              {/* <div className='modalDescription'>{this.props.event.description}</div> */}
-              <div className='modalDescription'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec suscipit quam. Praesent in nisi tellus. Suspendisse potenti. Sed sapien dolor, porttitor ac mattis tincidunt, finibus vel est. Ut ultricies justo blandit faucibus porta. Mauris ullamcorper lobortis ornare. Sed dolor arcu, pulvinar sit amet sem non, suscipit laoreet ex. Mauris rutrum, justo et scelerisque ornare, eros felis vestibulum dolor, et vestibulum elit arcu varius elit. Mauris dapibus ullamcorper placerat. Aenean condimentum vestibulum rhoncus. Mauris id commodo est, eget vehicula purus. Mauris bibendum, elit sit amet pellentesque ultrices, orci erat pharetra felis, in consectetur dui est et justo.</div>
+              <div className='modalDescription'>{this.props.event.description}</div>
+              {/* <div className='modalDescription'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec suscipit quam. Praesent in nisi tellus. Suspendisse potenti. Sed sapien dolor, porttitor ac mattis tincidunt, finibus vel est. Ut ultricies justo blandit faucibus porta. Mauris ullamcorper lobortis ornare. Sed dolor arcu, pulvinar sit amet sem non, suscipit laoreet ex. Mauris rutrum, justo et scelerisque ornare, eros felis vestibulum dolor, et vestibulum elit arcu varius elit. Mauris dapibus ullamcorper placerat. Aenean condimentum vestibulum rhoncus. Mauris id commodo est, eget vehicula purus. Mauris bibendum, elit sit amet pellentesque ultrices, orci erat pharetra felis, in consectetur dui est et justo.</div> */}
 
             </div>
           </Modal>
